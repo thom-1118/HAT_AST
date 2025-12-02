b1696533546aba8bd231915be72e24c973937913
He0he0 <dylantruong5@gmail.com>
2025-12-02 19:43:35 +0000

    Added the FRFN module from AST and swtiched out mlp for it.

--- hat/archs/hat_arch.py
+++ hat/archs/hat_arch.py
@@ -349,6 +349,52 @@ class PatchMerging(nn.Module):
         return x
 
 
+
+#FRFN module from AST Paper
+class FRFN(nn.Module):
+    def __init__(self, dim=32, hidden_dim=128, act_layer=nn.GELU,drop = 0., use_eca=False):
+        super().__init__()
+        self.linear1 = nn.Sequential(nn.Linear(dim, hidden_dim*2),
+                                act_layer())
+        self.dwconv = nn.Sequential(nn.Conv2d(hidden_dim,hidden_dim,groups=hidden_dim,kernel_size=3,stride=1,padding=1),
+                        act_layer())
+        self.linear2 = nn.Sequential(nn.Linear(hidden_dim, dim))
+        self.dim = dim
+        self.hidden_dim = hidden_dim
+
+        self.dim_conv = self.dim // 4
+        self.dim_untouched = self.dim - self.dim_conv 
+        self.partial_conv3 = nn.Conv2d(self.dim_conv, self.dim_conv, 3, 1, 1, bias=False)
+
+    def forward(self, x):
+        # bs x hw x c
+        bs, hw, c = x.size()
+        hh = int(math.sqrt(hw))
+
+
+        # spatial restore
+        x = rearrange(x, ' b (h w) (c) -> b c h w ', h = hh, w = hh)
+
+        x1, x2,= torch.split(x, [self.dim_conv,self.dim_untouched], dim=1)
+        x1 = self.partial_conv3(x1)
+        x = torch.cat((x1, x2), 1)
+
+        # flaten
+        x = rearrange(x, ' b c h w -> b (h w) c', h = hh, w = hh)
+
+        x = self.linear1(x)
+        #gate mechanism
+        x_1,x_2 = x.chunk(2,dim=-1)
+
+        x_1 = rearrange(x_1, ' b (h w) (c) -> b c h w ', h = hh, w = hh)
+        x_1 = self.dwconv(x_1)
+        x_1 = rearrange(x_1, ' b c h w -> b (h w) c', h = hh, w = hh)
+        x = x_1 * x_2
+        
+        x = self.linear2(x)
+        # x = self.eca(x)
+
+        return x
 class OCAB(nn.Module):
     # overlapping cross-attention block
 
@@ -392,6 +438,8 @@ class OCAB(nn.Module):
         self.norm2 = norm_layer(dim)
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=nn.GELU)
+        #defined frfn for forward pass
+        self.frfn = FRFN(dim=dim, hidden_dim=mlp_hidden_dim)
 
     def forward(self, x, x_size, rpi):
         h, w = x_size
@@ -446,7 +494,7 @@ class OCAB(nn.Module):
         x = self.proj(x) + shortcut
 
         #TODO: CHANGE THIS MLP TO BE THE FRFN FROM AST (maybe, we'll see)
-        x = x + self.mlp(self.norm2(x))
+        x = x + self.frfn(self.norm2(x))
         return x
 
 
